0c0d987bf9ce4e511426082d34a5bf507f663649
Jacob Witt <jbwitt11@gmail.com>
2023-02-28 19:32:08 -0700

    project init

---
+++ BLEDemo/BLEDemo/BLEDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEDemoApp.swift
+//  BLEDemo
+//
+//  Created by JAKE WITT on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct BLEDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
